f3379b745fcd6dfa222f3d5a78c91736d910e32b
omargueye88 <wwwgueyeomar25@gmail.com>
2025-04-12 01:44:45 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def process_video(input_video, output_video, model):
     out.release()
 
 # Routes Flask
-@app.route('/')
+@app.route('./app.py')
 def index():
     return render_template("index.html")
 
